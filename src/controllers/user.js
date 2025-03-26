63e7b8e92be2ac55f92da7ff9d2bfd5568050f23
Ana Paula Martins Ribeiro <ana.martins@cieemg.bhz>
2025-03-26 13:41:23 -0300

    feat(swagger): add settings swagger endpoint user

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -19,51 +19,84 @@ class UserController {
     }
 
     create = asyncHandler(async(req, res) => {
-        /*  #swagger.auto = false
+        /*
 
  test swagger
-
-        #swagger.path = '/users/{id}'
-        #swagger.method = 'put'
+        #swagger.tags = ['User']
+        #swagger.summary = 'Create a new user'
+        #swagger.description = 'This endpoint will create a new user'
+        #swagger.path = '/users/'
+        #swagger.method = 'post'
         #swagger.produces = ['application/json']
         #swagger.consumes = ['application/json']
-
-        #swagger.parameters['id'] = {
-            in: 'path',
-            description: 'User ID.',
-            required: true,
-            type: 'integer'
-        }
 
         #swagger.parameters['body'] = {
             in: 'body',
             description: 'User data.',
             required: true,
             schema: {
-                username: "user",
-                password: "1234"
+                name: "user",
+                idade: 18,
+                cpf: "xxx-xxx-xxx-xx",
+                email: "user@example.com",
+                password: "1234",
+                tipoUser: "CLIENTE",
+                descricao: "appointments",
+                cep: "xxxxx-xxx",
+                numero: 10,
+                rua: "Rua XYZ",
+                cidade: "Cidade ABC",
+                telefone: "xx-xxxxx-xxxx",
+                senhaAlterada: "false",
             }
         }
     */
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
+            // #swagger.responses[400] = { description: ''}
+            // #swagger.responses[404] = { description: ''}
+            // #swagger.responses[500] = { description: ''}
         }
 
         const novoUser = await UserService.create(req.body);
+        // #swagger.responses[201] = { description: 'User registered successfully.' }
         res.status(201).json(novoUser);
     });
 
     index = asyncHandler(async(req, res) => {
+
+        /*
+            #swagger.tags = ['User']
+            #swagger.summary = 'List all user'
+            #swagger.description = 'This endpoint will list all users'
+            #swagger.path = '/users/'
+            #swagger.method = 'get'
+*/
         const users = await UserService.findAll();
         res.json(users);
+        // #swagger.responses[200] = { description: 'List of users' }
+        // #swagger.responses[400] = { description: ''}
+        // #swagger.responses[404] = { description: ''}
+        // #swagger.responses[500] = { description: ''}
     });
 
     show = asyncHandler(async(req, res) => {
+        /*
+            #swagger.tags = ['User']
+            #swagger.summary = 'List a user'
+            #swagger.description = 'This endpoint will list a users'
+            #swagger.path = '/users/:id'
+            #swagger.method = 'get'
+*/
         const user = await UserService.findById(req.params.id);
         if (!user) {
             return res.status(404).json({ error: 'User não encontrado' });
         }
+        // #swagger.responses[200] = { description: 'List of user' }
+        // #swagger.responses[400] = { description: ''}
+        // #swagger.responses[404] = { description: 'User not found'}
+        // #swagger.responses[500] = { description: ''}
         res.json(user);
     });
 
